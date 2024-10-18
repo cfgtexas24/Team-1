089abe93da78cf41620310a5c298cb80f3874e34
Ethan Li <esli@gatech.edu>
2024-10-18 17:27:18 -0500

    initial login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Abide</h1>
+      <h2>create account</h2>
+      <form>
+        <input type="text" placeholder="Username" className="input-box" />
+        <input type="password" placeholder="Password" className="input-box" />
+        <button type="submit" className="submit-button">Create Account</button>
+      </form>
     </div>
   );
 }
