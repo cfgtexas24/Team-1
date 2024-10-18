8cfe6f351e2f6fcd261a62d3513c5ccf362600c0
Ayushi Sharma <ayushi.shrma00@gmail.com>
2024-10-18 18:33:19 -0500

    survey conflicts resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,27 @@ import SurveyForm from './components/SurveyForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Client Survey</h1>
-        <SurveyForm />
-      </header>
-    </div>
+    <div>
+      {/* First div for your container and form */}
+      <div className="container">
+        <h1>Abide</h1>
+        <form>
+          <input type="text" placeholder="Username" className="input-box" />
+          <input type="password" placeholder="Password" className="input-box" />
+          <button type="submit" className="submit-button">Create Account</button>
+          <button type="button" className="link-button">Already have an Account?</button>
+        </form>
+      </div> 
+      {/* Second div for your survey form */}
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to the Client Survey</h1>
+          <SurveyForm />
+        </header>
+      </div>
+    </div> // Closing the outermost div that wraps both
   );
 }
 
 export default App;
+
