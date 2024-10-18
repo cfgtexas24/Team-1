a4c257912b38c3d5352e897780b1d994085a6d24
Ethan Li <esli@gatech.edu>
2024-10-18 17:53:00 -0500

    working username login page for create account

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="container">
       <h1>Abide</h1>
-      <h2>create account</h2>
       <form>
         <input type="text" placeholder="Username" className="input-box" />
         <input type="password" placeholder="Password" className="input-box" />
         <button type="submit" className="submit-button">Create Account</button>
+        <button type="button" className="link-button">Already have an Account?</button>
       </form>
     </div>
   );
