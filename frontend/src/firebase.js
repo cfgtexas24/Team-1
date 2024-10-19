9df59535bade99908578092172de6d00d9bb8041
Ethan Li <esli@gatech.edu>
2024-10-19 03:28:26 -0500

    firebase auth

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "cfgteam1login.firebaseapp.com",
-  projectId: "cfgteam1login",
-  storageBucket: "cfgteam1login.appspot.com",
-  messagingSenderId: "1068313793699",
-  appId: "1:1068313793699:web:b04d55de4fbc5319761b16",
-  measurementId: "G-Q4VHFXSC67"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
