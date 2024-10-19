f48c30aed3fed83724d47a654e6d2de4481915d8
rh-2022 <rh3128@columbia.edu>
2024-10-18 20:31:56 -0500

    Temporary changes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './components/Home';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
+import Login from'./components/Login';
+import LoginExisting from './components/LoginExistingUser'
 import PatientDashboard from './components/PatientDashboard';
 import ProviderDashboard from './components/ProviderDashboard';
-import AdminDashboard from './components/AdminDashboard'; // Import the AdminDashboard component
-import './components/AdminDashboard.css';
-import SurveyForm from './components/SurveyForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
         <Route path="/patient/login" element={<Login />} />
-        <Route path="/patient/home" element={<PatientDashboard />} />
-        {/* Standalone Admin Dashboard Route */}
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/patient/home" element={<PatientDashboard />}/>
+        <Route path="/patient/login/existing" element={<LoginExisting />} />
         <Route path="/provider-dashboard" element={<ProviderDashboard />} />
-        <Route path="/patient-dashboard" element={<PatientDashboard />} />
-        <Route path="/survey-form" element={<SurveyForm />} />
-
       </Routes>
+
     </BrowserRouter>
   );
 }
