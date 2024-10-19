936925bbc8d2e6f9a03ca5bbe5973ee0a26a8385
LerichO <Lerich.Osay@UTDallas.edu>
2024-10-18 23:06:35 -0500

    some more patient dash stuff

--- frontend/src/components/PatientDashboard.js
+++ frontend/src/components/PatientDashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+
+import LabReport from './common/LabReport'
 
 // todo:
 // list latest labs report
-// list latest appointment
+// list upcoming appointment
 // form to request a new appointment
 // survey sent by provider
 // calendar of latest appointments
@@ -11,48 +13,67 @@ import { useState, useEffect } from 'react'
 
 const PatientDashboard = () => {
 
+  // useRef for sending scren down to certain sections
+  const notificationsRef = useRef(null);
+  const labReportsRef = useRef(null);
+  const appointmentsRef = useRef(null);
+  const calendarRef = useRef(null);
+
+  const scrollTo = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: 'smooth'
+    })
+  }
+
+  // store patient state
   const [patient, setPatient] = useState(
     {
-      firstName: "Jane",
-      lastName: "Doe",
+      name: "Jane Doe",
       age: 28,
     }
   )
 
-  const [labs, setLabs] = useState(['lab 1 file text here :)', 'lab 2 file text here again'])
+  // state for lab objects
+  const [labs, setLabs] = useState(['lab 1 text here :)', 'lab 2 text here again'])
 
-  const [latestAppointment, setLatestAppointment] = useState(
+  // upcoming appointment information
+  const [upcomingAppointment, setUpcomingAppointment] = useState(
     {
       date: '08/08/2025',
       info: 'appointment info goes here'
-  
+
     }
   )
 
-  const [input, setInput] = useState();
+  const [surveyQuestion, setSurveyQuestion] = useState('');
+  const [surveyAnswer, setSurveryAnswer] = useState('');
 
   // useEffect();
 
   return (
     <div className='flex flex-row w-screen h-screen'>
-      <div className="flex h-full w-1/4 bg-slate-700 justify-center items-center text-white">
-        <h1>Hello, <span><b> {patient.firstName} {patient.lastName}</b></span></h1>
+      <div className="flex flex-col h-full w-1/4 bg-slate-700 justify-center items-center text-white">
+        <h1>Hello, <span><b> {patient.name}</b></span></h1>
+        <ul className='flex flex-col gap-8'>
+          <li><button onClick={() => scrollTo(notificationsRef)}>Notifications</button></li>
+          <li><button onClick={() => scrollTo(labReportsRef)}>Lab Report</button></li>
+          <li><button onClick={() => scrollTo(appointmentsRef)}>Appointments</button></li>
+          <li><button onClick={() => scrollTo(calendarRef)}>Calendar</button></li>
+        </ul>
       </div>
-      <div className="flex flex-col h-full w-full bg-gray-300 p-8">
-        <h2 className='font-bold text-xl'>Good day! Here are your latest lab reports:</h2>
-        <div className='flex flex-row'>
-          {labs.map((lab, index) => (
-            <div key={index} className='p-2'>
-              {lab}
-            </div>
+      <div id="content" className="flex flex-col h-full w-full bg-gray-300 p-8">
+        <h2 ref={notificationsRef} className='font-bold text-xll'>Notifications</h2>
+        <h2 ref={labReportsRef} className='font-bold text-xl'>Your Latest lab reports:</h2>
+        <div className='flex flex-col'>
+          {labs.map(lab => (
+            <LabReport lab={lab} />
           ))}
         </div>
-        <h2 className='text-xl pt-8'>You have an upcoming appointment at <span><b>{latestAppointment.date}</b></span> with the following information below</h2>
-        <p>{latestAppointment.info}</p>
+        <h2 ref={appointmentsRef} className='text-xl pt-8'>You have an upcoming appointment at <span><b>{upcomingAppointment.date}</b></span> with the following information below</h2>
+        <p>{upcomingAppointment.info}</p>
+        <h2 ref={labReportsRef} className='font-bold text-xl'>Calendar</h2>
       </div>
-      <form>
-        <label></label>
-      </form>
     </div>
   )
 }
