156def1d5751ca7e13f63e856be3cd4968891b2c
duyenemily <duyenemily.nguyen@gmail.com>
2024-10-18 23:23:29 -0500

    Patient Demographic Info Endpoint

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.post('/verify-token', async (req, res) => {
   }
 });
 
-// Fetch patient data by ID
+/*// Fetch patient data by ID
 app.get('/patients/:id', async (req, res) => {
     const patientId = req.params.id;
     
@@ -58,7 +58,7 @@ app.get('/patients/:id', async (req, res) => {
       console.error('Error fetching patient data:', error);
       res.status(500).json({ error: 'Error fetching patient data' });
     }
-});
+});*/
 
 // Update patient data by ID
 app.put('/patients/:id', async (req, res) => {
@@ -155,33 +155,25 @@ app.get('/providers/patient-records', async (req, res) => {
 });
 
 // Patient View Information
-
-// Specific Patient Medical Records
-app.get('/patients/medical-records', async (req, res) => {
-  let username = {};
+app.get('/patients/demographics', async (req, res) => {
+  let name = "";
   try {
-    username = req.body;
-    console.log(username);
+    name = req.body.name;
+    console.log(name);
   } catch(error) {
     console.error('Error fetching patients username:', error);
     res.status(500).json({ error: 'Error patients username' });
   }
   try {
-    const userID = await db.collection('patients').get();
-    //const labReports = await db.collection('medicalRecords').get();
-    //const patients = snapshot.docs.map(doc => doc.data());
+    const recordRef = db.collection('patients').doc(name);
+    const doc = await recordRef.get();
+
+    if (!doc.exists) {
+      return res.status(404).json({ message: 'No demographic data found for this patient' });
+    }
+
+    res.json(doc.data());
   
-    res.status(200).json(userID);
-  } catch(error) {
-    console.error('Error fetching data from database:', error);
-    res.status(500).json({ error: 'Error fetching data from database' });
-  }
-});
-
-// Patient Lab Report
-app.get('/patient/lab-report', async (req, res) => {
-  try{
-
   } catch(error) {
     console.error('Error fetching data from database:', error);
     res.status(500).json({ error: 'Error fetching data from database' });
