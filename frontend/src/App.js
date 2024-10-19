9bbd175277cbe104d03ed0486729c7c9b50dc0f5
Ethan Li <esli@gatech.edu>
2024-10-18 20:36:23 -0500

    admin dashboard prelim stuff using recharts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './components/Home';
-import SignUp from'./components/SignUp';
-import Login from './components/Login'
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 import PatientDashboard from './components/PatientDashboard';
+import AdminDashboard from './components/AdminDashboard'; // Import the AdminDashboard component
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/patient/login" element={<Login />} />
-        <Route path="/patient/home" element={<PatientDashboard />}/>
+        <Route path="/patient/home" element={<PatientDashboard />} />
         <Route path="/patient/signup" element={<SignUp />} />
+        {/* Standalone Admin Dashboard Route */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
