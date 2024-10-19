3e661d1dcb161bdd1c24ef346f8a375ca768484e
LerichO <Lerich.Osay@UTDallas.edu>
2024-10-18 19:03:20 -0500

    fixed file structure, router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './components/Home';
+import Login from'./components/Login'
 
 function App() {
   return (
-    <div className="container">
-      <h1>Abide</h1>
-      <form>
-        <input type="text" placeholder="Username" className="input-box" />
-        <input type="password" placeholder="Password" className="input-box" />
-        <button type="submit" className="submit-button">Create Account</button>
-        <button type="button" className="link-button">Already have an Account?</button>
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/patient/login" element={<Login />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
