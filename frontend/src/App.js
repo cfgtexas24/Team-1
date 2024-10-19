d5a85b155ddc1f12ca85b64c8a82a5ab7616dc45
rh-2022 <rh3128@columbia.edu>
2024-10-19 02:21:37 -0500

    Update App.js to include frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './components/Home';
-import Login from'./components/Login';
-import LoginExisting from './components/LoginExistingUser'
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 import PatientDashboard from './components/PatientDashboard';
 import ProviderDashboard from './components/ProviderDashboard';
+import AdminDashboard from './components/AdminDashboard'; // Import the AdminDashboard component
+import PatientList from './components/PatientList'; //up
+import './AdminDashboard.css';
+import SurveyForm from './components/SurveyForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SignUp />} />
         <Route path="/patient/login" element={<Login />} />
-        <Route path="/patient/home" element={<PatientDashboard />}/>
-        <Route path="/patient/login/existing" element={<LoginExisting />} />
-        <Route path="/provider-dashboard" element={<ProviderDashboard />} />
+        <Route path="/patient/home" element={<PatientDashboard />} />
+        {/* Standalone Admin Dashboard Route */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/patient-list" element={<PatientList />} />
+        <Route path="/survey-form" element={<SurveyForm />} />
+        <Route path="/provider-dashboard/:id" element={<ProviderDashboard />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
